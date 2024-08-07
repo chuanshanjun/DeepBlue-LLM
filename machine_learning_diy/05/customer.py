94e963d2c0677f19010c0714a53c3c4f537b532e
chuanshanjun <chuanshanjun@gmail.com>
2024-08-07 22:31:01 +0800

    更新神经网络内容

--- machine_learning_diy/05/customer.py
+++ machine_learning_diy/05/customer.py
@@ -60,11 +60,11 @@ print("在未做特征工程的前提下，"
 # 单隐层神经网络Keras实现
 # 使用序惯模型构建网络
 import keras
-from keras import Sequential
-from keras.layers import Dense # 导入Keras 全连接层
+from keras.src.models import Sequential
+from keras.src.layers import Dense # 导入Keras 全连接层
 
 ann = Sequential() # 创建一个序贯ANN模型
-ann.add(Dense(units=12, input_dim=11, activation='relu')) # 添加输入层
+ann.add(Dense(units=12, input_dim=12, activation='relu')) # 添加输入层
 ann.add(Dense(units=24, activation='relu'))               # 添加隐层
 ann.add(Dense(units=1, activation='sigmoid'))             # 添加输出层
 ann.summary() # 显示网络模型(这个语句不是必须的)
@@ -73,5 +73,42 @@ ann.summary() # 显示网络模型(这个语句不是必须的)
 from IPython.display import SVG # 实现神经网络结构的图形化显示
 from keras.src.utils.model_visualization import model_to_dot
 
+svg = SVG(model_to_dot(ann, show_shapes=True).create(prog='dot', format='svg'))
 
-SVG(model_to_dot(ann, show_shapes=True).create(prog='dot', format='svg'))
+with open('ann.svg', 'w') as f:
+    f.write(svg.data)
+
+# 编译神经网络，指定优化器，损失函数，以及评估指标
+ann.compile(optimizer='adam',             # 优化器
+            loss='binary_crossentropy',   # 损失函数
+            metrics=['acc'])              # 评估指标
+
+history = ann.fit(X_train, y_train,                 # 指定训练集
+                  epochs=30,                        # 指定轮次
+                  batch_size=64,                    # 指定批量大小
+                  validation_data=(X_test, y_test)) # 指定验证集
+
+def show_history(history): # 显示训练过程中的学习曲线
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(loss) + 1)
+    plt.figure(figsize=(12,4))
+    plt.subplot(1,2,1)
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    plt.subplot(1,2,2,)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+show_history(history)

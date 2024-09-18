d9a036ab02207794355bf4935d63cc70c8d50fb7
chuanshanjun <chuanshanjun@gmail.com>
2024-09-18 23:18:40 +0800

    再来一遍transformer

--- LLM_diy/07_Transformer/transformer_02.py
+++ LLM_diy/07_Transformer/transformer_02.py
@@ -1,2 +1,73 @@
 import numpy as np
 import torch
+import torch.nn as nn
+
+d_k = 64
+d_v = 64
+
+# 缩放点积注意力类
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, Q, K, V, attn_mask):
+        # 计算注意力分数
+        scores = torch.matmul(Q, K.transpose(-1, -2))/np.sqrt(d_k)
+
+        # 使用注意力掩码，将attn_mask中值为1的位置权重替换为极小值
+        scores.masked_fill_(attn_mask, -1e9)
+
+        # 使用softmax函数对注意力分数进行归一化
+        weights = nn.Softmax(dim=-1)(scores)
+
+        # 计算上下文向量（也就是注意力的输出），是上下文信息的紧凑表示
+        context = torch.matmul(weights, V)
+
+        return context, weights
+
+
+# 多头自注意力
+d_embedding = 512
+n_headers = 8
+batch_size = 3
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.W_Q = nn.Linear(d_embedding, d_k * n_headers)
+        self.W_K = nn.Linear(d_embedding, d_k * n_headers)
+        self.W_V = nn.Linear(d_embedding, d_v * n_headers)
+        self.linear = nn.Linear(n_headers * d_v, d_embedding)
+        self.layer_norm = nn.LayerNorm(d_embedding)
+
+    def forward(self, Q, K, V, attn_mask):
+        # 保留残差连接
+        residual, batch_size = Q, Q.size(0)
+
+        # 将输入进行线性变换和重塑，以便后续处理
+        # 自注：我这要重塑的话，每个seq中的每个词(token原来是512维度的现在我只拿到其中的一部分就是64维)
+        q_s = self.W_Q(Q).view(batch_size, -1, n_headers, d_k).transpose(1, 2)
+        k_s = self.W_K(K).view(batch_size, -1, n_headers, d_k).transpose(1, 2)
+        v_s = self.W_V(V).view(batch_size, -1, n_headers, d_v).transpose(1, 2)
+
+        # 将注意力掩码复制到多头
+        # 自注：说白了，我现在的维度是，batch_size, n_headers, seq_len, d_k
+        # 因为n_headers在前面，但seq_len在后面，所以我按照n_headers维度填充，知道不同seq_len的填充不同的内容
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_headers, 1, 1)
+
+        # 使用缩放点积注意力计算上下文和注意力权重
+        context, weight = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
+
+        # 通过调整维度将多个头的上下文向量连接在一起
+        context.trnaspose(1, 2).contiguous().view(batch_size, -1, n_headers * d_v)
+
+        # 用一个线性层把连接后的多头自注意力结果转换为原始的嵌入维度
+        # 自注：这么做的一个可能的原因是，我的context的维度是dim_v与dim_q不同，所以可能要转回去?
+        # 还有个可能，线性变换后，张量的形状与Q相同，我就可以做下面的残差连接操作了
+        output = self.linear(context)
+
+        # 与输入(Q)进行残差连接，进行层归一化的操作
+        # 在每个样本的特征维上进行归一化，也就是对每个token的embedding维度进行归一化
+        output = self.layer_norm(output + residual)
+
+        # 返回层归一化的输出和注意力权重
+        return output, weight
